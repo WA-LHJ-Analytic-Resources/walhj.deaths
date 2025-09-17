ace42eed8fbee5a1c52febd59eb621a5048520d6
Tyler Bonnell <Tyler.Bonnell@co.snohomish.wa.us>
2025-09-16 17:06:19 -0700

    Add crosswalk_death_data() skeleton

---
+++ R/crosswalk_death_data.R
@@ -0,0 +1,13 @@
+#' Crosswalk Death Data File Years
+#'
+#' @description Death Certificate Statistical files changed considerably after the Washington State Department of Health changed collection systems
+#' from the Electronic Death Registration System (EDRS) to the Washington Health and Life Event Systems (WHALES). Specifically, the death certificate data
+#' is considerably different from 2010-2015 versus 2016-onwards and requires considerable reformatting to preserve longitudinal analysis across these time periods.
+#'
+#' @param df
+#' @param y
+#' @param z
+#' @return A data frame
+#' @export
+
+crosswalk_death_data <- function(df) {}
