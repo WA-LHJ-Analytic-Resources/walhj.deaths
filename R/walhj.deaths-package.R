c3733724de7b6418641a9a335a87495d355a7c3e
Tyler Bonnell <Tyler.Bonnell@co.snohomish.wa.us>
2025-10-02 15:25:26 -0700

    Update documentation and NAMESPACE

---
+++ R/walhj.deaths-package.R
@@ -0,0 +1,21 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dplyr arrange
+#' @importFrom dplyr filter
+#' @importFrom dplyr mutate
+#' @importFrom dplyr pull
+#' @importFrom dplyr slice_min
+#' @importFrom fs dir_ls
+#' @importFrom fs path_file
+#' @importFrom here here
+#' @importFrom janitor clean_names
+#' @importFrom rio import
+#' @importFrom stringr str_detect
+#' @importFrom stringr str_extract
+#' @importFrom stringr str_sub
+#' @importFrom tibble as_tibble
+#' @importFrom tidyr separate_wider_delim
+## usethis namespace: end
+NULL
